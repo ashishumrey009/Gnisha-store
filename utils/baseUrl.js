831744b9dfdc27449066031389f6761265911159
ashishumrey009 <ashishumrey009@gmail.com>
2020-06-24 16:24:41 +0530

    server

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,3 +1,3 @@
-const baseUrl = process.env.NODE_ENV === 'production' ? 'https://ginisha-store.now.sh' : 'http://localhost:3000'
-export default baseUrl
-
+//const baseUrl = process.env.NODE_ENV === 'production' ? 'http://localhost:3000' : 'http://localhost:3000'
+const baseUrl = "http://localhost:" + process.env.PORT || 3000
+export default baseUrl;
