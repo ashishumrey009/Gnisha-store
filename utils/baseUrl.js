c6b0418ed2ffbb66dafbdb1a2eb9d35d8e142c39
ashishumrey009 <ashishumrey009@gmail.com>
2020-06-24 17:24:06 +0530

    base

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,3 +1,4 @@
-const baseUrl = "http://0.0.0.0:" + process.env.PORT;
+//const baseUrl = "http://0.0.0.0:" + process.env.PORT;
 //const baseUrl = "http://localhost:3000"
+const baseUrl = "https://ginisha-store.herokuapp.com"
 export default baseUrl;
